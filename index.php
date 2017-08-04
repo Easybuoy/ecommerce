ae895afd218b7981644ca9631c417fff28b0ba57
Ekunola Ezekiel <ekunolaeasybuoy@gmail.com>
2017-08-04 07:34:48 +0100

    [feature] ecommerce

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <html lang="en">
     <head>
       <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+<!--      <meta name="viewport" content="width=device-width, initial-scale=1>-->
       <title>Quest Store</title>
         <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
         <link rel="stylesheet" type="text/css" href="css/main.css">
@@ -17,18 +17,38 @@
 <!-- Top Nav Bar -->
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
-      <a href="#" class="navbar-brand">Quest Store</a>
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#myNavbar" aria-expanded="false">
+<!--                <span class="sr-only">Toggle Navigation </span>-->
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="" class="navbar-brand"><img src="images/logo2.png" style="max-width: 150px; margin-top: -7px;"> </a>
+        </div>
+<div class="collapse navbar-collapse" id="myNavbar">
+    <div class="navbar-header">
       <ul class="nav navbar-nav">
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
+          <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Men<span class="caret"></span></a>
+              <ul class="dropdown-menu role="menu">
           <li><a href="#">Shirts</a></li>
           <li><a href="#">Pants</a></li>
           <li><a href="#">Shoes</a></li>
           <li><a href="#">Accessories</a></li>
           </li>
-        </ul>
       </ul>
+        </ul>
+
+    </div>
+    <div class="navbar-header navbar-right">
+        <ul class="nav navbar-nav">
+            <li><a href="">Signin</a> </li>
+            <li><a href="">Signup</a> </li>
+        </ul>
+    </div>
+</div>
+
 
     </div>
 </nav>
